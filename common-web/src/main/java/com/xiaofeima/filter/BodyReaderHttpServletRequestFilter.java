8d0d31aca4a246efe9198abe35afc36a8f32cd69
lipiaogit <1825596570@qq.com>
2020-07-19 14:34:43 +0800

    body提交

---
+++ common-web/src/main/java/com/xiaofeima/filter/BodyReaderHttpServletRequestFilter.java
@@ -0,0 +1,42 @@
+package com.shengsu.filter;
+
+/**
+ * Created by Bell on 2019/12/11.
+ */
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author Bell
+ */
+public class BodyReaderHttpServletRequestFilter implements Filter {
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        ServletRequest requestWrapper = null;
+        if (request instanceof HttpServletRequest) {
+            requestWrapper = new BodyReaderHttpServletRequestWrapper((HttpServletRequest) request);
+        }
+        if (null == requestWrapper) {
+            chain.doFilter(request, response);
+        } else {
+            chain.doFilter(requestWrapper, response);
+        }
+    }
+
+    /**
+     * 初始化函数时，需要获取排除在外的url
+     */
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+}
